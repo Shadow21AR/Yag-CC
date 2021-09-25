8866b75d9a2a93b5d12706df11598a9d50e4539c
Shadow <66721908+Shadow21AR@users.noreply.github.com>
2021-09-25 18:22:38 +0530

    Update reaction-bj.go

--- BlackJack-cc/reaction-bj.go
+++ BlackJack-cc/reaction-bj.go
@@ -1,7 +1,10 @@
+{{/* trigger type : Reaction - Added reaction only */}}
+
 {{ if .ReactionMessage.Embeds }}
     {{ if and ( $data := (dbGet .User.ID "bj").Value ) (in (((index .Message.Embeds 0).Footer.Text)) (str .User.ID)) (eq .Message.ID (toInt $data.msg_id)) }}
         {{ $embed := structToSdict (index .Message.Embeds 0) }}
         {{ $deck := $data.deck }}
+        {{ $amount := $data.amount }}
         {{ $emojis := $data.emojis }}
         {{ $msg := (toInt $data.msg_id) }}
         {{ $p_cards := $data.p_cards }}
@@ -38,12 +41,13 @@
             {{end}}
             {{ deleteMessageReaction nil .Message.ID .User.ID "hit:874954948801073163" }}
             {{ $embed.Set "description" (printf "%s's Hand:\n%s\n%s\nTotal: %d\n\nDealer's Hand:\n%s\n%s\nTotal: ??" .User.String (joinStr " " $p_t.StringSlice) (joinStr " " $p_b.StringSlice) $p_total (joinStr " " $d_ht.StringSlice) (joinStr " " $d_hb.StringSlice) ) }}
-            {{ dbSetExpire .User.ID "bj" (sdict "deck" $deck "p_cards" $p_cards "d_cards" $d_cards "p_total" $p_total "d_total" $d_total "score" $score "p_t" $p_t "p_b" $p_b "d_t" $d_t "d_b" $d_b "d_ht" $d_ht "d_hb" $d_hb "emojis" $emojis "msg_id" (str $msg)) 180 }}
+            {{ dbSetExpire .User.ID "bj" (sdict "amount" $amount "deck" $deck "p_cards" $p_cards "d_cards" $d_cards "p_total" $p_total "d_total" $d_total "score" $score "p_t" $p_t "p_b" $p_b "d_t" $d_t "d_b" $d_b "d_ht" $d_ht "d_hb" $d_hb "emojis" $emojis "msg_id" (str $msg)) 180 }}
             {{ if gt $p_total 21 }}
                 {{ $embed.Set "color" 0xFF0000 }}
                 {{ $embed.Set "title" "You Busted" }}
                 {{ deleteAllMessageReactions nil .Message.ID }}
-                {{ $embed.Set "description" (printf "%s's Hand:\n%s\n%s\nTotal: %d\n\nDealer's Hand:\n%s\n%s\nTotal:  %d " .User.String (joinStr " " $p_t.StringSlice) (joinStr " " $p_b.StringSlice) $p_total (joinStr " " $d_t.StringSlice) (joinStr " " $d_b.StringSlice) $d_total) }}
+                {{ $embed.Set "description" (printf "%s's Hand:\n%s\n%s\nTotal: %d\n\nDealer's Hand:\n%s\n%s\nTotal:  %d \n\nYou lost `%d` Credits." .User.String (joinStr " " $p_t.StringSlice) (joinStr " " $p_b.StringSlice) $p_total (joinStr " " $d_t.StringSlice) (joinStr " " $d_b.StringSlice) $d_total $amount) }}
+                {{ $notNice := dbIncr .User.ID "CREDIT" (mult $amount -1) }}
                 {{ dbDel .User.ID "bj" }}
             {{ end }}
         {{ else if eq .Reaction.Emoji.ID 874954815736807454 }}
@@ -72,19 +76,25 @@
             {{ if gt $d_total 21 }}
                 {{ $embed.Set "color" 0x00FF00 }}
                 {{ $embed.Set "title" "Dealer Busted" }}
+                {{ $embed.Set "description" (printf "%s's Hand:\n%s\n%s\nTotal: %d\n\nDealer's Hand:\n%s\n%s\nTotal:  %d\n\nYou won `%d` Credits." .User.String (joinStr " " $p_t.StringSlice) (joinStr " " $p_b.StringSlice) $p_total (joinStr " " $d_t.StringSlice) (joinStr " " $d_b.StringSlice) $d_total $amount) }}
+               {{ $nice := dbIncr .User.ID "CREDIT" (mult 2 $amount) }}
             {{ else }}
                 {{ if gt $p_total $d_total }}
                     {{ $embed.Set "color" 0x00FF00 }} 
                     {{ $embed.Set "title" "You Won!" }}
+                   {{ $embed.Set "description" (printf "%s's Hand:\n%s\n%s\nTotal: %d\n\nDealer's Hand:\n%s\n%s\nTotal:  %d\n\nYou won `%d` Credits." .User.String (joinStr " " $p_t.StringSlice) (joinStr " " $p_b.StringSlice) $p_total (joinStr " " $d_t.StringSlice) (joinStr " " $d_b.StringSlice) $d_total $amount) }}
+                  {{ $nice := dbIncr .User.ID "CREDIT" (mult 2 $amount) }}
                 {{ else if lt $p_total $d_total }}
                     {{ $embed.Set "color" 0xFF0000 }}
                     {{ $embed.Set "title" "You Lost!" }}
+                {{ $embed.Set "description" (printf "%s's Hand:\n%s\n%s\nTotal: %d\n\nDealer's Hand:\n%s\n%s\nTotal:  %d\n\nYou Lost `%d` Credits." .User.String (joinStr " " $p_t.StringSlice) (joinStr " " $p_b.StringSlice) $p_total (joinStr " " $d_t.StringSlice) (joinStr " " $d_b.StringSlice) $d_total $amount) }}
+               {{ $nice := dbIncr .User.ID "CREDIT" (mult $amount -1) }}
                 {{ else if eq $p_total $d_total }}
                     {{ $embed.Set "color" 0xFFFFFF }}
                     {{ $embed.Set "title" "It's a Tie!" }}
+                   {{ $embed.Set "description" (printf "%s's Hand:\n%s\n%s\nTotal: %d\n\nDealer's Hand:\n%s\n%s\nTotal:  %d\n\nNobody won anything lol" .User.String (joinStr " " $p_t.StringSlice) (joinStr " " $p_b.StringSlice) $p_total (joinStr " " $d_t.StringSlice) (joinStr " " $d_b.StringSlice) $d_total) }}
                 {{ end }}
             {{ end }}
-            {{ $embed.Set "description" (printf "%s's Hand:\n%s\n%s\nTotal: %d\n\nDealer's Hand:\n%s\n%s\nTotal:  %d " .User.String (joinStr " " $p_t.StringSlice) (joinStr " " $p_b.StringSlice) $p_total (joinStr " " $d_t.StringSlice) (joinStr " " $d_b.StringSlice) $d_total) }}
             {{ deleteAllMessageReactions nil .Message.ID }}
             {{ dbDel .User.ID "bj" }}
         {{ end }}
