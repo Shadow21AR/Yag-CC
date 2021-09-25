5272abcc3fe96fe867b977cd8917347ccf3dcccb
Shadow <66721908+Shadow21AR@users.noreply.github.com>
2021-09-25 18:23:38 +0530

    Update main-bj.go

--- BlackJack-cc/main-bj.go
+++ BlackJack-cc/main-bj.go
@@ -1,10 +1,26 @@
-{{/* Recommend trigger type : command
-     Trigger : bj
+{{/* Recommend trigger type : regex
+     Trigger : `\A(?:\-|<@!?204255221017214977>)\s*(?:bj|blackjack)(?: +|\z)`
      Copyright (c): Shadow21A, 2021
      License: MIT
      Repository: https://github.com/Shadow21AR/Yag-CC
 */}}
 
+{{/* Reaction Emojis */}}
+{{ $hitEmoji := "hit:874954948801073163" }}
+{{ $stayEmoji := "stay:874954815736807454" }}
+
+{{ $args := parseArgs 0 "__**Usage:**__ -bj <amount>" (carg "int" "Amount") }}
+{{ $amount := 0 }}
+{{ if $args.IsSet 0 }}
+		{{ $amount = $args.Get 0 }}
+{{ end }}
+{{ $ack := (dbGet .User.ID "CREDIT").Value | toInt }}
+{{ if and ( gt $amount $ack) }}
+{{ sendMessage nil (print "Not enough credits!!!\nCheck balance with `-bal` \nYou can play without any amount by `bj` / `blackjack`." )}} 
+{{/* Admins can set credits with -setb @user <amount> */}}
+{{ else if lt $amount 0 }}
+No.
+{{ else }}
 {{ if not .ExecData }}
     {{ if $db := (dbGet .User.ID "bj") }}
         {{ sendMessage nil (printf "%s | Please finish your previous game.\nCan't find it??\nHere: %s" .User.Mention (getMessage $db.Value.channel_id $db.Value.msg_id).Link ) }}
@@ -19,7 +35,8 @@
         {{ $d_total := 0 }} {{ $d_score := 0 }}
         {{/* Emojis */}}
         {{ $emojis := dict "spade" "<:espades:874957677577515090>" "heart" "<:ehearts:874957690974126120>" "diamond" "<:ediamonds:874957704962146314>" "club" "<:eclubs:874957717318557706>" "rqueen" "<:rQ:874957256511348826>" "rking" "<:rK:874957294494973973>" "rjoker" "<:rJ:874957308818501705>" "race" "<:rA:874957319623036928>" "r10" "<:r10:874957647646965790>" "r9" "<:r9:874957434903461908>" "r8" "<:r8:874957444382593024>" "r7" "<:r7:874957454440558592>" "r6" "<:r6:874957463835787284>" "r5" "<:r5:874957576079540264>" "r4" "<:r4:874957602835025925>" "r3" "<:r3:874957616432939068>" "r2" "<:r2:874957632585236511>" "bqueen" "<:bQ:874957337704673321>" "bking" "<:bK:874957355949908019>" "bjoker" "<:bJ:874957367551336450>" "bace" "<:blackA:874958863575687188>" "b10" "<:b10:874958052682514433>" "b9" "<:b9:874957839121129482>" "b8" "<:b8:874957850957475870>" "b7" "<:b7:874957860684058715>" "b6" "<:b6:874957874537840650>" "b5" "<:b5:874957947745226762>" "b4" "<:b4:874957964044283924>" "b3" "<:b3:874957975213715457>" "b2" "<:b2:874957987435929620>" "tblank" "<:blankbacktop:874958066699882516>" "bblank" "<:blankbackbot:874958077714108436>" }}
-        {{/* Shuffling deck */}}
+		
+		{{/* Shuffling deck */}}
         {{ $n := 0 }}
         {{ range seq 0 4 }}
             {{- range seq 0 13 }}
@@ -27,10 +44,13 @@
             {{- end }}
             {{- $n = add $n 1 -}}
         {{ end }}
-        {{ $deck = shuffle $deck }}
-        {{/* Serving 2 cards to Player and Dealer | calculating scores */}}
-        {{/* Player */}}
-        {{ range seq 0 2 }}
+		{{ $deck = shuffle $deck }}
+        
+		{{/* Serving 2 cards to Player and Dealer | calculating scores */}}
+        
+		{{/* For Player */}}
+        
+		{{ range seq 0 2 }}
             {{- $card := index $deck 0 -}}
             {{- $p_cards = ($p_cards.Append $card ) -}}
             {{- $deck = slice $deck 1 -}}
@@ -42,8 +62,10 @@
             {{ end }}
             {{- $p_total = add $p_score $p_total -}}
         {{ end }}
-        {{/* Dealer */}}
-        {{ range seq 0 2 }}
+        
+		{{/* For Dealer */}}
+        
+		{{ range seq 0 2 }}
             {{- $card := index $deck 0 -}}
             {{- $d_cards = ($d_cards.Append $card ) -}}
             {{- $deck = slice $deck 1 -}}
@@ -55,7 +77,9 @@
             {{ end }}
                 {{- $d_total = add $d_score $d_total -}}
         {{ end }}
+
         {{/* Getting equivalent emojis */}}
+
         {{ $p_t := cslice }} {{ $p_b := cslice }} {{ $d_t := cslice }} {{ $d_b := cslice }} {{ $p_score := "" }}
         {{ range $p_cards }}
             {{- if reFind `spade|club` . -}}
@@ -83,6 +107,7 @@
         {{ if eq $p_total 21 }}
             {{ $title = "You Got BLACKJACK!!" }}
             {{ $color = 0x00FF00 }}
+			{{ $nice := dbIncr .User.ID "CREDIT" (mult 5 $amount) }}
         {{ end }}
         {{ $embed := cembed 
         "title" $title
@@ -93,8 +118,8 @@
         "timestamp" currentTime }}
         {{ if lt $p_total 21 }}
             {{ $msg := sendMessageRetID nil $embed }}
-            {{ addMessageReactions nil $msg "hit:874954948801073163" "stay:874954815736807454" }}
-            {{ dbSetExpire .User.ID "bj" (sdict "deck" $deck "p_cards" $p_cards "d_cards" $d_cards "p_total" $p_total "d_total" $d_total "score" $score "p_t" $p_t "p_b" $p_b "d_t" $d_t "d_b" $d_b "d_ht" $d_ht "d_hb" $d_hb "emojis" $emojis "msg_id" (str $msg ) "channel_id" (str .Channel.ID ) ) 180 }}
+            {{ addMessageReactions nil $msg $hitEmoji $stayEmoji }}
+            {{ dbSetExpire .User.ID "bj" (sdict "amount" $amount "deck" $deck "p_cards" $p_cards "d_cards" $d_cards "p_total" $p_total "d_total" $d_total "score" $score "p_t" $p_t "p_b" $p_b "d_t" $d_t "d_b" $d_b "d_ht" $d_ht "d_hb" $d_hb "emojis" $emojis "msg_id" (str $msg ) "channel_id" (str .Channel.ID ) ) 180 }}
 
             {{ execCC .CCID nil 175 (str $msg) }}
         {{ end }}
@@ -109,3 +134,4 @@
         {{ dbDel .User.ID "bj" }}
     {{ end }}
 {{end}}
+{{ end }}
