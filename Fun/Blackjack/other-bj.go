5003ff04b8a4a262fc965a67ca2d7684ec2d0683
Shadow <66721908+Shadow21AR@users.noreply.github.com>
2021-11-16 11:02:14 +0530

    Update other-bj.go
    added `-giveb @user <amount>` command.

--- Fun/Blackjack/other-bj.go
+++ Fun/Blackjack/other-bj.go
@@ -1,27 +1,34 @@
 {{/* Trigger type : regex.
-     Trigger : \A(?:\-|<@!?204255221017214977>)\s*(?:bal|setb|setbalance)(?: +|\z) */}}
+     Trigger : \A(?:\-|<@!?204255221017214977>)\s*(?:bal|setb|giveb)(?:\s+|\z) */}}
 
-
-{{ $args := parseArgs 0 "__To check balance__ : `-bal [@user]Optional`\n__To set balance__: `-setb @user <amount>`" 
- (carg "member" "user") 
- (carg "int" "amount") }}
+{{ $args := parseArgs 0 "__To check balance__ : `-bal [@user]Optional`\n__To set balance__: `-setb @user <amount>`\n__To add balance__: `-giveb @user <amount>`" 
+	(carg "member" "user") 
+	(carg "int" "amount") }}
+ {{$perms := "Administrator"}}
 {{ $user := .User }}
 {{ $value := 0 }}
 {{ if $args.IsSet 0 }}
- {{ $user = ($args.Get 0).User }}
+	{{ $user = ($args.Get 0).User }}
 {{ end }}
 {{ if $args.IsSet 1 }}
- {{ $value = $args.Get 1 | toInt }}
+	{{ $value = $args.Get 1 | toInt }}
 {{ end }}
 {{ if eq (lower .Cmd) "-bal" }}
- {{ sendMessage nil (print $user.Mention "'s balance is : " (or (toInt (dbGet $user.ID "CREDIT").Value) 0)) }}
-{{ else }}
- {{/* Black Wolf's permission checker here ;) */}}
- {{$perms := "Administrator"}}
-{{if (in (split (index (split (exec "viewperms") "\n") 2) ", ") $perms)}}
-	{{ dbSet $user.ID "CREDIT" $value }}
-       {{ sendMessage nil (print "Success!\nUpdated " $user.Mention " balance to " $value "." ) }}
-{{else}}
-	{{sendMessage nil (print "You can't use this command\nYou need atleast one of these perms: " $perms) }}
-{{end}}
+	{{ sendMessage nil (print $user.Username "'s balance is : " (or (toInt (dbGet $user.ID "CREDIT").Value) 0)) }}
+{{ else if eq (lower .Cmd) "-setb" }}
+	 {{/* Black Wolf's permission checker here ;) */}}
+	{{if (in (split (index (split (exec "viewperms") "\n") 2) ", ") $perms)}}
+		{{ dbSet $user.ID "CREDIT" $value }}
+		{{ sendMessage nil (print "Success!\nUpdated " $user.Username " balance to " $value "." ) }}
+	{{else}}
+		{{sendMessage nil (print "You can't use this command\nYou need at least one of these perms: " $perms) }}
+	{{end}}
+{{ else if eq (lower .Cmd) "-giveb" }}
+	 {{/* Black Wolf's permission checker here again ;) */}}
+	{{if (in (split (index (split (exec "viewperms") "\n") 2) ", ") $perms)}}
+		{{ $new := dbIncr $user.ID "CREDIT" $value }}
+		{{ sendMessage nil (print "Success!\nUpdated " $user.Username " balance to " $new "." ) }}
+	{{else}}
+		{{sendMessage nil (print "You can't use this command\nYou need at least one of these perms: " $perms) }}
+	{{end}}
 {{ end }}
