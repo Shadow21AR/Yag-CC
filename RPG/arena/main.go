f9606026a4b63ad3aee66040054e6dd6554b9bff
Shadow <66721908+Shadow21AR@users.noreply.github.com>
2021-12-21 14:25:06 +0530

    Update Arena code to match MB
    Fixed list, they wont disappear now
    fixed response/trigger deletion

--- RPG/arena/main.go
+++ RPG/arena/main.go
@@ -5,22 +5,21 @@
 
 {{$expiryTime := 15}} {{/* time in minutes to reset arena list after being inactive */}}
 {{$cookie := "🍪"}}
-{{$alistcd := 3}} {{/* delay in SECONDS */}}
 
 {{/* ----- don't edit if u don't know what you doing -------*/}}
 {{/* Copyright (c): Shadow21A, 2021
      License: MIT
      Repository: https://github.com/Shadow21AR/Yag-CC
 */}}
+{{$list := ""}}{{$count := 1}}{{$msg := ""}}
+{{$alist := cslice.AppendSlice (or (dbGet .Channel.ID "alist").Value cslice)}}
+{{$ex := or (and (reFind "a_" .Guild.Icon) "gif" ) "png" }}
+{{$icon := print "https://cdn.discordapp.com/icons/" .Guild.ID "/" .Guild.Icon "." $ex "?size=1024" }}
+{{$helpM := "```\n• a join  : To join the list\n• a leave : To leave the list\n• a list  : To view the list```"}}
+{{$embed := sdict "author" (sdict "name" (print .Guild.Name) "icon_url" $icon) "timestamp" currentTime }}
 {{if not .ExecData}}
-	{{$list := ""}}{{$count := 1}}{{$msg := ""}}
-	{{$alist := cslice.AppendSlice (or (dbGet .Channel.ID "alist").Value cslice)}}
-	{{$ex := or (and (reFind "a_" .Guild.Icon) "gif" ) "png" }}
-	{{$icon := print "https://cdn.discordapp.com/icons/" .Guild.ID "/" .Guild.Icon "." $ex "?size=1024" }}
-	{{$helpM := "```\n• a join  : To join the list\n• a leave : To leave the list\n• a list  : To view the list```"}}
-	{{$embed := sdict "author" (sdict "name" (print .Guild.Name) "icon_url" $icon) "timestamp" currentTime }}
-
- {{if and .CmdArgs (eq $join .Channel.ID)}}
+	{{if and .CmdArgs (eq $join .Channel.ID)}}
+		{{deleteTrigger 1}}{{deleteResponse 1}}
 		{{$cmd := index .CmdArgs 0 | lower}}
 		{{if and (eq $cmd "join") (le (len $alist) 10)}}
 			{{if not (in $alist .User.ID)}}
@@ -29,7 +28,7 @@
 				{{range $alist}}
 					{{- $list = printf "%s`[%02d]` <@%d>\n" $list $count .}}{{$count = add 1 $count -}}
 				{{end}}
-				{{$embed.Set "description" (print $list "\n" $helpM "List are sent to <#" $arena ">")}}{{$embed.Set "title" (print "🍪 __" .Guild.Name " Arena List [" (len $alist) "/10]__ 🍪")}}
+				{{$embed.Set "description" (print $list "\n" $helpM "List are sent to <#" $arena ">")}}{{$embed.Set "title" (print $cookie "__Arena List [" (len $alist) "/10]__" $cookie)}}
 				{{if $smsg := dbGet .Channel.ID "smsg"}}{{deleteMessage nil $smsg.Value 1}}{{end}}
 				{{$msg = sendMessageRetID nil (cembed $embed)}}
 				{{dbSet .Channel.ID "smsg" (str $msg)}}
@@ -48,6 +47,9 @@
 					{{$fembed := cembed "description" (printf "```\nRpg arena %s```" $list1) "footer" (sdict "text" "Note to host: Dont forget to remove your ID.")}}
 					{{sendMessage $arena $list1}}
 					{{addMessageReactions $arena (sendMessageRetID $arena $fembed) $cookie}}
+					{{$embed.Set "description" (print $helpM "List are sent to <#" $arena ">")}}{{$embed.Set "title" (print $cookie "__" .Guild.Name " Arena List [0/10]__" $cookie)}}
+					{{$msg = sendMessageRetID nil (cembed $embed)}}
+					{{dbSet .Channel.ID "smsg" (str $msg)}}
 				{{end}}
 			{{else}}
 				{{deleteMessage nil (sendMessageRetID nil "you are already in list") 2}}
@@ -63,7 +65,7 @@
 				{{range $new}}
 					{{- $list = printf "%s`[%02d]` <@%d>\n" $list $count .}}{{$count = add 1 $count -}}
 				{{end}}
-				{{$embed.Set "description" (print $list "\n" $helpM "List are sent to <#" $arena ">")}}{{$embed.Set "title" (print "🍪 __" .Guild.Name " Arena List [" (sub (len $alist) 1) "/10]__ 🍪")}}
+				{{$embed.Set "description" (print $list "\n" $helpM "List are sent to <#" $arena ">")}}{{$embed.Set "title" (print $cookie "__" .Guild.Name " Arena List [" (sub (len $alist) 1) "/10]__" $cookie)}}
 				{{if $smsg := dbGet .Channel.ID "smsg"}}{{deleteMessage nil $smsg.Value 1}}{{end}}
 				{{$msg = sendMessageRetID nil (cembed $embed)}}
 				{{dbSet .Channel.ID "smsg" (str $msg)}}
@@ -72,32 +74,36 @@
 			{{else}}
 				{{deleteMessage nil (sendMessageRetID nil "you are not in list") 3}}
 			{{end}}
+		{{else if eq $cmd "hide"}}
+			{{removeRoleID $arenaRole}}
 		{{else if eq $cmd "list"}}
 			{{range $alist}}
 				{{- $list = printf "%s`[%02d]` <@%d>\n" $list $count .}}{{$count = add 1 $count -}}
 			{{end}}
-			{{$embed.Set "description" (print $list "\n" $helpM "List are sent to <#" $arena ">")}}{{$embed.Set "title" (print "🍪 __" .Guild.Name " Arena List [" (len $alist) "/10]__ 🍪")}}
-{{if $lmsg := dbGet .Channel.ID "lmsg"}}{{deleteMessage nil $lmsg.Value 1}}{{end}}
-			{{deleteMessage nil (sendMessageRetID nil (cembed $embed)) $alistcd}}
-		{{else if eq $cmd "hide"}}
-			{{removeRoleID $arenaRole}}
+			{{$embed.Set "description" (print $list "\n" $helpM "List are sent to <#" $arena ">")}}{{$embed.Set "title" (print $cookie "__" .Guild.Name " Arena List [" (len $alist) "/10]__" $cookie)}}
+			{{if $smsg := dbGet .Channel.ID "smsg"}}{{deleteMessage nil $smsg.Value 1}}{{end}}
+			{{$msg = sendMessageRetID nil (cembed $embed)}}
+			{{dbSet .Channel.ID "smsg" (str $msg)}}
 		{{else if eq $cmd "reset"}}
 			{{if hasRoleID $mod}}
 				{{dbDel .Channel.ID "alist"}}
 			 	{{if $smsg := dbGet .Channel.ID "smsg"}}{{deleteMessage nil $smsg.Value 1}}{{end}}
 				{{dbDel .Channel.ID "smsg"}}
 				{{cancelScheduledUniqueCC .CCID "alist"}}
-				{{deleteMessage nil (sendMessageRetID nil "Deleted the list!") 5}}
+				{{$embed.Set "description" (print $helpM "List are sent to <#" $arena ">")}}{{$embed.Set "title" (print $cookie "__" .Guild.Name " Arena List [0/10]__" $cookie)}}
+				{{$msg = sendMessageRetID nil (cembed $embed)}}
+				{{dbSet .Channel.ID "smsg" (str $msg)}}
 			{{else}}
 				{{deleteMessage nil (sendMessageRetID nil "You don't have permissions to reset list!") 5}}
 			{{end}}
 		{{end}}
-		{{deleteTrigger 2}}
 	{{end}}
 {{else}}
 	{{deleteMessage nil .ExecData.msg 1}}
 	{{dbDel .Channel.ID "alist"}}
 	{{dbDel .Channel.ID "smsg"}}
-	{{deleteMessage nil (sendMessageRetID nil (print "Arena got reset due to inactivity!\nNo player joined the list for past " $expiryTime " minutes.")) 30}}
+	{{$embed.Set "description" (print $helpM "List are sent to <#" $arena ">")}}{{$embed.Set "title" (print $cookie "__" .Guild.Name " Arena List [0/10]__" $cookie)}}
+	{{$msg = sendMessageRetID nil (cembed $embed)}}
+	{{dbSet .Channel.ID "smsg" (str $msg)}}
 {{end}}
 {{/* --------------- End of code --------------- */}}
